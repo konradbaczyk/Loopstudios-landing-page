ae3186d794f2067e49f92cabecc8c6816ca85186
Konrad01~ <konradbc7@gmail.com>
2024-09-03 20:25:35 +0200

    Initial commit. Created responsive header seciton with navbar.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Menu items
+const btnToggle = document.getElementById("btn-toggle-menu");
+const navMenu = document.getElementById("menu");
+
+const handleMenu = (e) => {
+	e.preventDefault();
+	if (navMenu.classList.contains("navigation__menu--active")) {
+		btnToggle.style.backgroundImage = "url(../../images/icon-hamburger.svg)";
+		document.body.classList.remove("no-scroll");
+	} else {
+		btnToggle.style.backgroundImage = "url(../../images/icon-close.svg)";
+		document.body.classList.add("no-scroll");
+	}
+
+	navMenu.classList.toggle("navigation__menu--active");
+};
+
+btnToggle.addEventListener("click", handleMenu);
