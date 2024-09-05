4b4084887d72e89a65d07b8c904e206e9fe20e07
Konrad01~ <konradbc7@gmail.com>
2024-09-05 12:57:43 +0200

    Corrected paths to images, added screenshots, changed README

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ const navMenu = document.getElementById("menu");
 const handleMenu = (e) => {
 	e.preventDefault();
 	if (navMenu.classList.contains("navigation__menu--active")) {
-		btnToggle.style.backgroundImage = "url(../../images/icon-hamburger.svg)";
+		btnToggle.style.backgroundImage = "url(images/icon-hamburger.svg)";
 		document.body.classList.remove("no-scroll");
 	} else {
-		btnToggle.style.backgroundImage = "url(../../images/icon-close.svg)";
+		btnToggle.style.backgroundImage = "url(images/icon-close.svg)";
 		document.body.classList.add("no-scroll");
 	}
 
